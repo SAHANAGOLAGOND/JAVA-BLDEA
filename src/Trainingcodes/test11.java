cdc3e99fb8da6a57e153592cffe044bc98d0a00c
91814 <91814@sahana123>
2023-03-20 21:13:45 +0530

    13th commit

--- src/Trainingcodes/test11.java
+++ src/Trainingcodes/test11.java
@@ -1,10 +1,26 @@
 package Trainingcodes;
 
+import java.util.Scanner;
+
 public class test11 {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the age:");
+		int age=sc.nextInt();
+		if(age<18) {
+			System.out.println("your are still minor please growup");
+		}
+		else if(age>65) {
+			System.out.println("you are senior citizen");
+		}
+		else {
+			System.out.println("congratulations");
+		}
+
+	}
+
 		// TODO Auto-generated method stub
 
 	}
 
-}
---
+++ src/Trainingcodes/test12.java
@@ -0,0 +1,10 @@
+package Trainingcodes;
+
+public class test12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
