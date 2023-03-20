05cff72e9229f36a4e3e4436435b65964c9683d0
91814 <91814@sahana123>
2023-03-20 21:00:55 +0530

    7th commit

--- src/Trainingcodes/test5.java
+++ src/Trainingcodes/test5.java
@@ -1,10 +1,22 @@
 package Trainingcodes;
 
+import java.util.Scanner;
+
 public class test5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the values of n");
+		int n=sc.nextInt();
+		int count=n*n;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+				System.out.print(count+" ");
+				count--;
+			}
+			System.out.println();
+		}
+	}
 
 	}
 
-}
