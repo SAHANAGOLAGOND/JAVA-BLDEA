f27cf0ef77677e8b9888f869544633cd7b0b3e52
91814 <91814@sahana123>
2023-03-20 20:58:44 +0530

    6th commit

--- src/Trainingcodes/test4.java
+++ src/Trainingcodes/test4.java
@@ -1,10 +1,17 @@
 package Trainingcodes;
-
+import java.util.Scanner;
 public class test4 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter the values of n");
+	int n=sc.nextInt();
+	int count=1;
+	for(int i=1;i<=n;i++) {
+		for(int j=1;j<=n;j++) {
+			System.out.print(count+" ");
+			count++;
+		}
+		System.out.println();
 	}
-
+}
 }
---
+++ src/Trainingcodes/test5.java
@@ -0,0 +1,10 @@
+package Trainingcodes;
+
+public class test5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
