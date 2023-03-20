4ce0ba02703574c48c8bcdec02c8ba6bd4588bd9
91814 <91814@sahana123>
2023-03-20 20:54:31 +0530

    5th commit

--- src/Trainingcodes/test3.java
+++ src/Trainingcodes/test3.java
@@ -1,10 +1,23 @@
 package Trainingcodes;
-
-public class test3 {
-
+import java.util.Scanner;
+public class test3{
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the value of n:");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
 
+
+			for (int j=1;j<=n;j++) {
+				if (i==1 || i==n || j==1 || j==n) {
+					System.out.print("$");		
+				}
+				else {
+					System.out.print("- ");
+				}
+			}
+
+			System.out.println();
+		}
 	}
-
 }
