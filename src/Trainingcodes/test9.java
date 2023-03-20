702f5f32d06e93bc4dfb3a7e13e4a6b44e947c01
91814 <91814@sahana123>
2023-03-20 21:08:10 +0530

    10th commit

--- src/Trainingcodes/test9.java
+++ src/Trainingcodes/test9.java
@@ -1,17 +1,10 @@
 package Trainingcodes;
 
-public class test8 {
+public class test9 {
 
 	public static void main(String[] args) {
-		for(int i=1;i<=5;i++) {
-			System.out.print("i");
-		}
-
-	}
-
-
 		// TODO Auto-generated method stub
 
 	}
 
-
+}
