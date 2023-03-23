baccaa09778c10ec1095660bef92a1315483daca
91814 <91814@sahana123>
2023-03-23 19:17:10 +0530

    21st commit

--- src/Trainingcodes/test15.java
+++ src/Trainingcodes/test15.java
@@ -1,10 +1,19 @@
 package Trainingcodes;
 
+import java.util.Arrays;
+
 public class test15 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int arr[]
+				= {10,30,25,68,40,15,90,78,144,71,54,8};
+						Arrays.sort(arr);
+					for(int i=0;i<arr.length;i++) {
+						System.out.print(arr[i]+", ");
+					}
+				}
 
-	}
+				}
 
-}
+		
+
