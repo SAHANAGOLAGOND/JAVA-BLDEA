8fe4e6faf12306acb1ce8a6cb2b1219b6fde55ef
91814 <91814@sahana123>
2023-03-23 19:15:33 +0530

    20th commit

--- src/Trainingcodes/Arraycode2.java
+++ src/Trainingcodes/Arraycode2.java
@@ -1,10 +1,33 @@
 package Trainingcodes;
 
+import java.util.Scanner;
+
 public class Arraycode2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("enter count of classes:");
+		Scanner sc=new Scanner(System.in);
+		int cls=sc.nextInt();
+		System.out.println("enter the count of students in each class:");
+		int stu=sc.nextInt();
+		int arr[][]=new int[cls][stu] ;
+		for(int i=0;i<cls;i++) {
+			System.out.println("inside the class"+ (i+1));
+			for(int j=0;j<stu;j++) {
+				System.out.println("enter the marks of student"+(j+1));
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		System.out.println("-----------");
+		for(int i=0;i<cls;i++) {
+			System.out.println("inside the class"+(i+1));
+			for(int j=0;j<stu;j++) {
+				System.out.println("the marks of student"+(j+1)+"is:"+arr[i][j]);
+			}
+		}
 
+		
+		
 	}
 
 }
