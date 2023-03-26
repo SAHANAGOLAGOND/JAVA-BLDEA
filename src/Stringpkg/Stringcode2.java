25fd527babcbd3478238a9cc26167715ee1411c6
91814 <91814@sahana123>
2023-03-26 20:43:11 +0530

    Count the number of vowels and cons

---
+++ src/Stringpkg/Stringcode2.java
@@ -0,0 +1,10 @@
+package Stringpkg;
+
+public class Stringcode2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/Trainingcodes/test16.java
+++ src/Trainingcodes/test16.java
@@ -1,10 +1,30 @@
 package Trainingcodes;
 
+import java.util.Arrays;
+
 public class test16 {
 
 	public static void main(String[] args) {
+		int arr1[]= {10,30,25,68,40,15,90,78,144,71,54,8};
+		int arr2[]= {90,78,10,30,25,144,71,54,8,68,40,888};
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		if(arr1.length==arr2.length) {
+			for (int i=0;i<arr1.length;i++) {
+				if(arr1[i]!=arr2[i]) {
+					System.out.println("Arrays not equal");
+					System.exit(0);
+				}
+			}
+			System.out.println("Arrays are equal");
+		}
+		else {
+			System.out.println("Arrays not qual");
+		}
+		
+	}
+
+
 		// TODO Auto-generated method stub
 
 	}
-
-}
