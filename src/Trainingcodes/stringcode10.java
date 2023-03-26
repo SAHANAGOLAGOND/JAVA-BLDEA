76ba3c52d38d9590313e6de41a9ddcbfca8287a6
91814 <91814@sahana123>
2023-03-26 22:52:51 +0530

    commit27

--- src/Trainingcodes/stringcode10.java
+++ src/Trainingcodes/stringcode10.java
@@ -1,10 +1,30 @@
 package Trainingcodes;
 
+import java.util.Scanner;
+
 public class stringcode10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the String-1");
+		String Str1=sc.next();
+		System.out.println("enter the String-2");
+		String Str2=sc.next();
+		if(Str1.equalsIgnoreCase(Str2)==true) {
+			System.out.println("Strings are equal");
+
+		}
+		else {
+			System.out.println("String are not equal");
+		
+}
+
 
 	}
 
 }
+
+	
+
+
