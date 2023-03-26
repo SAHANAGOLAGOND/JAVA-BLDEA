f4a78d2733c6936a571f2f4fe27f2893e5762817
91814 <91814@sahana123>
2023-03-26 22:46:52 +0530

    commit26

---
+++ src/Trainingcodes/stringcode9.java
@@ -0,0 +1,31 @@
+package Trainingcodes;
+
+public class stringcode9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubString Str="RajaRamMohanRoy";
+		String Str="RajaRamMohanRoy";
+		System.out.println(Str.length());
+		System.out.println(Str.toUpperCase());
+		System.out.println(Str.toLowerCase());
+		System.out.println(Str.charAt(1));
+		int a=10+Str.charAt(1);
+		System.out.println(a);
+		System.out.println(Str.indexOf('R'));
+		System.out.println(Str.indexOf('Z'));
+		System.out.println(Str.startsWith("Raja"));
+		System.out.println(Str.startsWith("Roja"));
+		System.out.println(Str.endsWith("Roy"));
+		System.out.println(Str.endsWith("Boy"));
+		System.out.println(Str.contains("Mohan"));
+		System.out.println(Str.contains("Rohan"));
+		System.out.println(Str.contains("mohan"));
+
+
+
+
+	}
+
+}
+
+		
