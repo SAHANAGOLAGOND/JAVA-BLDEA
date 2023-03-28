43d7939267ea269974fb2f866ba90e5b6d785319
91814 <91814@sahana123>
2023-03-28 21:32:00 +0530

    commit30

---
+++ src/Trainingcodes/test17.java
@@ -0,0 +1,25 @@
+package Trainingcodes;
+
+public class test17 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1=new String("RAMA");
+		 String s2=new String("RAMA");
+		 if(s1==s2) {
+			 System.out.println("adrress equal");
+			 
+		 }
+			 else {
+				 System.out.println("adress are not equal");
+			 }
+				
+
+
+
+
+
+
+	}
+
+}
