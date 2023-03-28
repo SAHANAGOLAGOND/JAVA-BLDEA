66800416bbe04a81f9c97f9280b0b684343c97ee
91814 <91814@sahana123>
2023-03-28 22:08:30 +0530

    commit40

--- src/Trainingcodes/test25.java
+++ src/Trainingcodes/test25.java
@@ -1,10 +1,31 @@
 package Trainingcodes;
 
+import java.util.Scanner;
+
 public class test25 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		  System.out.println("Enter the string:");
+String str1=sc.next();
+str1=str1.toLowerCase();
+for(int i=0;i<str1.length();i++) {
+	if(str1.charAt(i)=='a' ||str1.charAt(i)=='e'|| str1.charAt(i)=='i' ||str1.charAt(i)=='o' || str1.charAt(i)=='u') {
+		System.out.print("@");
+	}
+
+else {
+	
+	System.out.print(str1.charAt(i));
+}
+}
+
 
 	}
 
-}
+
+
+	}
+
+
