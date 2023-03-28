f329453eb7a25198946921fcc4183dc01d8973c5
91814 <91814@sahana123>
2023-03-28 21:55:03 +0530

    commit35

--- src/Trainingcodes/test21.java
+++ src/Trainingcodes/test21.java
@@ -3,7 +3,21 @@ package Trainingcodes;
 public class test21 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String s1=new String("RAMA");
+		 String s2=new String("RAMA");
+		 if(s1.equals(s2)) {
+			 System.out.println("Strings equal");
+			 
+		 }
+			 else {
+				 System.out.println("Strings are not equal");
+			 }
+				
+
+
+
+
+
 
 	}
 
