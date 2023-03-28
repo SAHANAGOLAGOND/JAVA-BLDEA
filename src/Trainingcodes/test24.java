13e8cfae94fa64e19171779070ef8f67fced0d15
91814 <91814@sahana123>
2023-03-28 22:02:06 +0530

    commit37

--- src/Trainingcodes/test24.java
+++ src/Trainingcodes/test24.java
@@ -4,6 +4,20 @@ public class test24 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+				String s1="RAMA";
+				 String s2="RAMA";
+				 if(s1.equals(s2)) {
+					 System.out.println("adress are equal");
+					 
+				 }
+					 else {
+						 System.out.println("adress are equal");
+					 }
+						
+
+
+
 
 	}
 
