e02e1b71ceccf129000cc23fbb2b7621a75a0909
91814 <91814@sahana123>
2023-03-29 21:58:08 +0530

    commit32

--- src/Trainingcodes/test2.java
+++ src/Trainingcodes/test2.java
@@ -1,23 +1,10 @@
 package Trainingcodes;
-import java.util.Scanner;
-public class test2{
+
+public class test2 {
+
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter the value of n:");
-		int n=sc.nextInt();
-		for(int i=1;i<=n;i++) {
+		// TODO Auto-generated method stub
 
-
-			for (int j=1;j<=n;j++) {
-				if (i==1 || i==n || j==1 || j==n) {
-					System.out.print("$");		
-				}
-				else {
-					System.out.print("- ");
-				}
-			}
-
-			System.out.println();
-		}
 	}
+
 }
