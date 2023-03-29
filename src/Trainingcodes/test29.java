14abfabb3f9098cefe61400fd2fc8dd7dd6e62fc
91814 <91814@sahana123>
2023-03-29 22:03:21 +0530

    commit45

--- src/Trainingcodes/test29.java
+++ src/Trainingcodes/test29.java
@@ -4,6 +4,10 @@ public class test29 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String s1="Ravana";
+		String s2="Sita";
+		s1=s1.concat(s2);
+		System.out.println(s1);
 
 	}
 
