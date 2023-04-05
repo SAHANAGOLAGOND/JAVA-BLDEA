a1daca60b29dd52499fa0bb051f2879aee7505cb
91814 <91814@sahana123>
2023-04-05 18:47:25 +0530

    construction chain commit

---
+++ src/Trainingcodes/Launch1.java
@@ -0,0 +1,29 @@
+package Trainingcodes;
+
+
+class Animal extends Object{
+	Animal(){
+		super();
+		System.out.println("inside zero parameterized constructor");
+	}
+	Animal(int a){
+		super();
+		System.out.println("inside parameterized constructor");
+	}
+}
+class Cat extends Animal{
+	Cat(){
+		super();
+	}
+}
+
+public class Launch1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Cat C1=new Cat();
+
+
+	}
+
+}
