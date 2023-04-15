083c864b5088a31be61e6d6652ff05492e57238b
91814 <91814@sahana123>
2023-04-15 19:05:31 +0530

    Crorepathi Candidate commit1

--- packagecom/crorepathi/pkg2/Candidate.java
+++ packagecom/crorepathi/pkg2/Candidate.java
@@ -1,10 +1,68 @@
 package packagecom.crorepathi.pkg2;
-
 public class Candidate {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String name;
+	 private int age;
+	 private String city;
+	private String state;
+	
+	static int amount;
 
+	/**
+	 * @param name
+	 * @param age
+	 * @param city
+	 * @param state
+	 */
+	public Candidate(String name, int age, String city, String state) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.city = city;
+		this.state = state;
 	}
 
+	/**
+	 * @return the amount
+	 */
+	public int getAmount() {
+		return amount;
+	}
+
+	/**
+	 * @param amount the amount to set
+	 */
+	public static void setAmount(int amt) {
+		amount = amt;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+	
+	
 }
