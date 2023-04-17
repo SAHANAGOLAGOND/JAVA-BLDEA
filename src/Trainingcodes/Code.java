d8b28af04b70fbd0a722c5b778b491ad5ec60931
91814 <91814@sahana123>
2023-04-17 18:54:39 +0530

    throws commit

--- src/Trainingcodes/Code.java
+++ src/Trainingcodes/Code.java
@@ -1,10 +1,26 @@
 package Trainingcodes;
+import java.util.Scanner;
 
-public class Code {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+class Operations{
+	void function() throws ArithmeticException{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the numerator:");
+int num= sc.nextInt();
+System.out.println("enter the denominator:");
+int deno=sc.nextInt();
+int res= num/deno;
+System.out.println(res);
 	}
-
+}
+public class Code {
+public static void main(String[] args) {
+	try {
+		Operations op=new Operations();
+		op.function();
+	}
+		catch(ArithmeticException e) {
+			System.out.println("Excetion handled in main Thread");
+	}
+}
 }
